978b175346f0d30742c0a66579d9ed3edba2cd80
mehmetyasincirakpy <myasincirak@gmail.com>
2024-01-13 17:30:31 +0300

    editing user class

--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -8,6 +8,8 @@ namespace Entities.Concrete
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string eMail { get; set; }
-        public string Password { get; set; }
+        public byte PasswordHash { get; set; }
+        public byte PasswordSalt { get; set; }
+        public bool Status { get; set; }
     }
 }
