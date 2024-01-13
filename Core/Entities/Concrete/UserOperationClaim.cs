60a34e7531a9a4af9f0f5d1a0bdc867ee80faa9c
mehmetyasincirakpy <myasincirak@gmail.com>
2024-01-13 17:55:32 +0300

    Created UserOperationClaims

---
+++ Core/Entities/Concrete/UserOperationClaim.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Concrete
+{
+    public class UserOperationClaim : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+    }
+}
