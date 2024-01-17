3e03605b9a1ec795d46a3acaa49a02edf420d96c
mehmetyasincirakpy <myasincirak@gmail.com>
2024-01-17 14:22:31 +0300

    created JWT operations

---
+++ Core/Utilities/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,10 @@
+using Core.Entities.Concrete;
+using Entities.Concrete;
+
+namespace Core.Utilities.Security.JWT
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+    }
+}
