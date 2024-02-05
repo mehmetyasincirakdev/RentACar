45bcae53bc3bc80e677707f2112029e288a38270
Mehmet Yasin Çirak <19050646@ogrenci.ankara.edu.tr>
2024-02-06 00:17:57 +0300

    updated packages and fixed connection string

--- DataAccess/Concrete/EntityFramework/RentACarContext.cs
+++ DataAccess/Concrete/EntityFramework/RentACarContext.cs
@@ -7,7 +7,7 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=.;Database=RentACar;Trusted_Connection=true;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\ProjectModels;Database=RentACar;Trusted_Connection=true");
         }
 
         public DbSet<Car> Cars { get; set; }
