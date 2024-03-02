1caa885102b6f283c1d5129b9a955e893ebfdeae
mehmetyasincirakdev <myasincirak@gmail.com>
2024-03-03 01:33:45 +0300

    added caching operations

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -2,6 +2,7 @@
 using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
@@ -22,6 +23,7 @@ namespace Business.Concrete
 
         [SecuredOperation("admin,car.manager")]
         [ValidationAspect(typeof(CarValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Car car)
         {
             _carDal.Add(car);
@@ -33,7 +35,7 @@ namespace Business.Concrete
             _carDal.Delete(car);
             return new SuccessResult(Messages.CarDeleted);
         }
-        
+        [CacheAspect]
         public IDataResult<List<Car>> GetAll()
         {
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(), Messages.CarsListed);
@@ -53,12 +55,12 @@ namespace Business.Concrete
         {
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.ColorId == id), Messages.CarsListedByColorId);
         }
-
+        [CacheAspect]
         public IDataResult<List<Car>> GetCarsById(int id)
         {
             return new SuccessDataResult<List<Car>>(_carDal.GetAll(c => c.Id == id), Messages.CarsListed);
         }
-
+        
         public IResult Update(Car car)
         {
             _carDal.Update(car);
